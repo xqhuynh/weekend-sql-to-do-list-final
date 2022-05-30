d3b36b76621f86f902d9dd432fb85b86f19c73cf
Savon Huynh <xqhuynh@gmail.com>
2022-05-29 21:27:02 -0500

    Create GET request

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -1,3 +1,22 @@
 const express = require("express");
 const todoRouter = express.Router();
 const pool = require("../modules/pool");
+
+// DB Connectin in modules folder
+
+// GET request
+todoRouter.get("/", (req, res) => {
+  let queryText = `SELECT * FROM todo;`;
+  pool
+    .query(queryText)
+    .then((result) => {
+      // Sends back results to object
+      res.send(result.rows);
+    })
+    .catch((err) => {
+      console.log("Error getting tasks", err);
+      res.sendStatus(500);
+    });
+});
+
+module.exports = todoRouter;
